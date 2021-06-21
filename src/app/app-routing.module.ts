82789cae0dc2f8ee60dbc656975ab68a50e70a6b
Juan David Gómez <juandagomez3@gmail.com>
2021-06-20 22:31:20 -0500

    continue landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) }];
+const routes: Routes = [{ path: 'inicio', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) },
+                        { path: '**', redirectTo:'inicio', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
