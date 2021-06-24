e411c40e09db0aaa730508f2aede3da54f121892
Juan David Gómez <juandagomez3@gmail.com>
2021-06-24 00:25:14 -0500

    login, register and list pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{ path: 'inicio', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) },
                         { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+                        { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
+                        { path: 'list', loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule) },
                         { path: '**', redirectTo:'inicio', pathMatch: 'full'}];
 
 @NgModule({
