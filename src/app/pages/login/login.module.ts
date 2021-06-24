b546aac45e6e30d0a73bacea307418d448a260ab
Juan David Gómez <juandagomez3@gmail.com>
2021-06-24 00:25:10 -0500

    login, register and list pages

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ButtonComponent],
   imports: [
     CommonModule,
     LoginRoutingModule
